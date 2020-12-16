05f12a97e1ec5197672c7a78f2495b5d3a9e7584
ppm1111 <ppqqbbdd1111@gmail.com>
2020-12-16 11:35:10 +0800

    chapter 6

--- src/main/java/open/ppm1111/tdd/Dollar.java
+++ src/main/java/open/ppm1111/tdd/Dollar.java
@@ -1,7 +1,6 @@
 package open.ppm1111.tdd;
 
-public class Dollar {
-    private int amount;
+public class Dollar extends Money {
     
     Dollar(int amount) {
         this.amount = amount;
@@ -9,10 +8,5 @@ public class Dollar {
     
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
-    }
-    
-    public boolean equals(Object object) {
-        Dollar dollar = (Dollar) object;
-        return amount == dollar.amount;
     }
 }
