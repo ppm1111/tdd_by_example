3da1425eac7457d9dbd923ecf47a5799ba81349e
ppm1111 <ppqqbbdd1111@gmail.com>
2020-12-16 11:29:18 +0800

    chapter 5

--- src/test/java/open/ppm1111/tdd/TddExampleApplicationTests.java
+++ src/test/java/open/ppm1111/tdd/TddExampleApplicationTests.java
@@ -14,13 +14,20 @@ class TddExampleApplicationTests {
     public void testMultiple() {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
-        assertEquals(new Dollar(15), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
     
     @Test
     public void testEquals() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+    }
+    
+    @Test
+    public void testFrancMultiple() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 
 }
